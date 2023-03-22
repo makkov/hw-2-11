ddcc81ce3a8035a5856fdeddf1a446170b5d16f0
maks <maxim_86nva@mail.ru>
2023-03-22 18:07:35 +0500

    init project

---
+++ src/main/java/com/example/hw211/Hw211Application.java
@@ -0,0 +1,13 @@
+package com.example.hw211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw211Application.class, args);
+    }
+
+}
