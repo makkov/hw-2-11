40f69ab28da231c7ef28dfd3802f92b640ebe827
maks <maxim_86nva@mail.ru>
2023-03-22 18:50:29 +0500

    структура проекта

--- src/main/java/com/example/hw211/entities/Order.java
+++ src/main/java/com/example/hw211/entities/Order.java
@@ -1,18 +1,4 @@
 package com.example.hw211.entities;
 
 public class Order {
-
-    private int id;
-
-    public Order(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
